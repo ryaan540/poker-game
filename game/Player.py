f4c8ee13da35de6dfe197ecfe968d5ee3436e878
ryaan540! <rkinyua841@gmail.com>
2026-04-13 10:19:26 +0300

    worked on

--- game/Player.py
+++ game/Player.py
@@ -1,7 +1,60 @@
+import random
+import time
+
 class Player():
-    def __init__(self, type="pc", cards=[], amount=0, total_amount_bet=0, name=""):
+
+    def __init__(self, type="pc", cards=None, total_amount_bet=0, name="", amount=0):
         self.name = name
         self.type = type
-        self.cards = cards
+        self.cards = cards if cards is not None else []
         self.total_amount_bet = total_amount_bet
         self.amount = amount
+
+    def place_initial_bet(self):
+
+        while True:
+            amount = input(f"Place initial bet amount. Current amount is {self.amount}: ")
+
+            if amount.isdigit():
+                n = int(amount)
+
+                if n > 0 and n <= self.amount:
+                    self.amount -= n
+                    return n
+
+                print("Invalid amount entered.")
+                print(f"Amount must range from 1 to {self.amount}")
+                print("Try again")
+
+            else:
+                print(f"Enter a number as valid amount between 1 and {self.amount}")
+
+    def auto_match_or_raise(self, amount):
+        print("PC thinking what to do")
+        time.sleep(2)
+
+        to_do = random.randint(1, 2)
+        raise_amount = amount + random.randint(10, 250)
+
+        if raise_amount > self.amount:
+            to_do = 1
+
+        # 1 is match
+        if to_do == 1:
+            if self.amount >= amount:
+                self.amount -= amount
+                print(f"Matching your action. Bet {amount}")
+                return amount
+            else:
+                return 0
+
+        else:
+            self.amount -= raise_amount
+            print("I have a good feeling. I raise by", raise_amount)
+            return raise_amount
+
+    def update_amount_bet(self, amount):
+        self.total_amount_bet = self.total_amount_bet + amount
+
+    def reset_amount_bet(self):
+        self.total_amount_bet = 0
